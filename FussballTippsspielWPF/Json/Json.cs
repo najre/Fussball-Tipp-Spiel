0975aa3374d3e0bf38747ee62be8022a260517d4
najre <reimer-jan@gmx.de>
2018-03-12 21:34:41 +0100

    update

--- FussballTippsspielWPF/Json/Json.cs
+++ FussballTippsspielWPF/Json/Json.cs
@@ -10,21 +10,26 @@ using System.Windows;
 
 namespace FussballTippsspielWPF
 {
+    public static class Globals
+    {
+        public static string ordnerPath = @"C:\Users\jan52\source\repos\Fussball-Tipp-Spiel\Bundesliga_Daten\";
+    }
+    
     class Json
     {
         public string JsonRunterladen(string woche)
         {
-            string path2 = @"C:\Users\jan\Documents\Bundesliga_Ergebnisse\" + woche + ".json";
+            Globals.ordnerPath += woche + ".json";
             string path = "https://www.openligadb.de/api/getmatchdata/bl1/2017/" + woche;
-            if (!File.Exists(path2))
+            if (!File.Exists(Globals.ordnerPath))
             {
-                using (FileStream fs = File.Create(path2))
+                using (FileStream fs = File.Create(Globals.ordnerPath))
                 {
                     fs.Close();
                 }
                 WebClient Client = new WebClient();
-                Client.DownloadFile(path, @"C:\Users\jan\Documents\Bundesliga_Ergebnisse\" + woche + ".json");
-                string jsonString = GetJsonToString(path2, woche);
+                Client.DownloadFile(path, Globals.ordnerPath);
+                string jsonString = GetJsonToString(path: Globals.ordnerPath, woche: woche);
                 JToken parsedJson = JToken.Parse(jsonString);
                 var beautifiedJson = parsedJson.ToString(Newtonsoft.Json.Formatting.Indented);
                 WriteJson(beautifiedJson, woche);
@@ -41,7 +46,7 @@ namespace FussballTippsspielWPF
         {
             string jsonString = "";
 
-            System.IO.StreamReader streamReader = new StreamReader(@"C:\Users\jan\Documents\Bundesliga_Ergebnisse\" + woche + ".json");
+            System.IO.StreamReader streamReader = new StreamReader(Globals.ordnerPath);
 
             jsonString = streamReader.ReadToEnd();
             streamReader.Close();
@@ -50,14 +55,14 @@ namespace FussballTippsspielWPF
 
         private string ReadJson(string woche)
         {
-            string st = File.ReadAllText(@"C:\Users\jan\Documents\Bundesliga_Ergebnisse\" + woche + ".json");
+            string st = File.ReadAllText(Globals.ordnerPath);
             return st;
 
         }
         private void WriteJson(string jsonString, string woche)
         {
             
-            System.IO.StreamWriter streamWriter = new StreamWriter(@"C:\Users\jan\Documents\Bundesliga_Ergebnisse\" + woche + ".json");
+            System.IO.StreamWriter streamWriter = new StreamWriter(Globals.ordnerPath);
             streamWriter.Write(jsonString);
             streamWriter.Close();
         }
